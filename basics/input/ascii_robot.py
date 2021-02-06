0559505ec7146e6b16e8ddc8ff472f276a2c5638
beniewaku <78078820+beniewaku@users.noreply.github.com>
2021-02-06 02:03:04 +0000

    "Added code to display an ASCII robot with dynamic eyes."

---
+++ basics/input/ascii_robot.py
@@ -0,0 +1,20 @@
+print("please enter a character for the eye")
+eye = input()
+
+print(f"""
+    ______    
+   ( {eye}  {eye} )
+   (   -  )
+    ------ 
+       |
+       |
+   --------
+  |  |   | |
+  |  |   | |
+  |  |   | |
+     -----
+     |   |
+     |   |
+     |   |
+     |   |
+""")
