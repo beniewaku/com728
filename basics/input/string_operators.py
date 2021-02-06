23c03b8fac5bd88b9a4416e8258b2939af7655f3
beniewaku <78078820+beniewaku@users.noreply.github.com>
2021-02-06 18:53:01 +0000

    "Added code to demonstrate the use of string operators."

---
+++ basics/input/string_operators.py
@@ -0,0 +1,14 @@
+print("Please enter the number of lives:")
+lives = int(input())
+
+print("Please enter the energy level:")
+energy = int(input())
+
+print("Please enter the shield level:")
+shield = int(input())
+
+print("Health has been set.")
+print()
+print(f"Lives:  {'♥'* lives}")
+print(f"Energy: {'♦'* energy}")
+print(f"Shield: {'♦'* shield}")
