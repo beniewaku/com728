a3761da512fa953e36c3ea4bf6e9b8b6ccf4a68f
beniewaku <78078820+beniewaku@users.noreply.github.com>
2021-02-06 20:11:18 +0000

    "Created a basic input/output program."

---
+++ basics/input/review.py
@@ -0,0 +1,16 @@
+print("what is your name?")
+name=input()
+
+print("what time is the train?")
+time= input()
+
+print("thank you")
+print()
+
+print("i have one more question")
+print()
+print("the journey takes 5 hours what time will i arrive?")
+print()
+journey=input()
+
+print("thank you")
