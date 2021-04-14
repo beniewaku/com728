826cfb8c1f3adc67fef863cbf18db63c4594b9fe
beniewaku <78078820+beniewaku@users.noreply.github.com>
2021-04-14 15:09:27 +0100

    "Added code to retrieve data using multiple inner joins."

--- persistence/joins/database.py
+++ persistence/joins/database.py
@@ -47,7 +47,6 @@ def display_products_with_stock_levels():
 
 
 def display_product_supplier():
-
     db = sqlite3.connect("catalogue.db")
     cursor = db.cursor()
     sql = "SELECT product.name, supplier.name  " \
@@ -61,3 +60,27 @@ def display_product_supplier():
         print()
 
     db.close()
+
+
+# Modify your module database so that it contains an additional function named display_product_supplier_locations.
+# This function should take no parameters and should display each product with its supplier and the location of the
+# supplier. You should use suitable multiple inner joins to query the database.
+
+def display_product_supplier_locations():
+    db = sqlite3.connect("catalogue.db")
+    cursor = db.cursor()
+    sql = "SELECT product.name, supplier.name, location.city, location.country  " \
+          "FROM product " \
+          "INNER JOIN supplier ON product.supplier_id = supplier.id  " \
+          "INNER JOIN location ON supplier.location_id = location.id"
+    cursor.execute(sql)
+    records = cursor.fetchall()
+
+    for record in records:
+        print(f"Product: {record[0]}, Supplier: {record[1]}, Supplier Location: {record[2]}, {record[3]}")
+        print()
+
+    db.close()
+
+
+
--- persistence/joins/main.py
+++ persistence/joins/main.py
@@ -1,8 +1,3 @@
-# Module main.py. This module contains the following functions:
-#
-# A function named menu that displays a menu of options as shown below and returns an integer corresponding to the
-# option selected by the user:
-
 
 import database
 
@@ -10,6 +5,7 @@ def menu():
     print("Select one of the following options:")
     print("[1] Display stock levels")
     print("[2] Display suppliers")
+    print("[3] Display supplier locations")
 
 
     selected_option = int(input("Your selection choice: "))
@@ -21,6 +17,8 @@ def run():
         database.display_products_with_stock_levels()
     elif selected_option == 2:
         database.display_product_supplier()
+    elif selected_option == 3:
+        database.display_product_supplier_locations()
     else:
         print("Invalid selection... try again")
         run()
